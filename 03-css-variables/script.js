56073da398bc50ec457c61f406c06eadc4969479
Ash <gaikwadashish2477@gmail.com>
2026-02-11 13:24:23 +0530

    feat: add touch support for mobile value changes

--- 03-css-variables/script.js
+++ 03-css-variables/script.js
@@ -4,14 +4,22 @@ let spacingInput = document.querySelector("#spacing");
 let blurInput = document.querySelector("#blur");
 let colorInput = document.querySelector("#color");
 
-spacingInput.addEventListener("mousemove", () => {
+function changeSpacing() {
     root.style.setProperty('--padding', `${spacingInput.value}px`);
-})
+}
 
-blurInput.addEventListener("mousemove", () => {
+function changeFilter() {
     root.style.setProperty('--blur', `${blurInput.value}px`);
-})
+}
 
-colorInput.addEventListener("change", () => {
+function changeColor() {
     root.style.setProperty('--base', `${colorInput.value}`); 
-})
+}
+
+spacingInput.addEventListener("mousemove", changeSpacing);
+spacingInput.addEventListener("touchmove", changeSpacing);
+
+blurInput.addEventListener("mousemove", changeFilter);
+blurInput.addEventListener("touchmove", changeFilter);
+
+colorInput.addEventListener("change", changeColor);
